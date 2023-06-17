894e4c0d5ef013e1b10bd719c7686b823b06e896
mmothiba4 <mmothiba4@gmail.com>
2023-06-17 11:28:48 -0700

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 /**
-*main – Entry point
-*
-return: Always 0 (Success)
-*/
+ * main - Entry point
+ * Return: 0 (Success)
+ */
 int main(void)
 {
-printf(“/"Programming is like building a multilingual puzzle”);
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
 
