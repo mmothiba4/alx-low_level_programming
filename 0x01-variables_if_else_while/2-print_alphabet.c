8e4f509b729aafedda45ff6eae686c46f108f6a8
mmothiba4 <mmothiba4@gmail.com>
2023-06-19 05:39:14 -0700

    task2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -3,4 +3,18 @@
 /**
  * main - Prints the alphabet.
  *
- * Return: Always 0n
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(alp[i]);
+	}
+	putchar('\n');
+	return (0);
+}
+
